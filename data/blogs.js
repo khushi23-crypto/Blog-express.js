ab2f46fd5d98cce0e2a2d828ac66b51046ac0eb5
Kajal Saini <sizukabts91@gmail.com>
2025-09-17 22:15:28 +0530

    Create BlogPage.html

--- data/blogs.js
+++ data/blogs.js
@@ -1,19 +1,23 @@
 blogs=[
     {
         title:"How to get started with Python",
-        content:"Python is a versatile programming language that is great for beginners and experts alike."
+        content:"Python is a versatile programming language that is great for beginners and experts alike.",
+        slug:"python-learn"
     },
     {
         title:"How to get started with JS",
-        content:"JS is a versatile programming language that is great for beginners and experts alike."
+        content:"JS is a versatile programming language that is great for beginners and experts alike.",
+        slug:"js-learn"
     },
     {
         title:"How to get started with React",
-        content:"React is a versatile framework that is great for beginners and experts alike. "
+        content:"React is a versatile framework that is great for beginners and experts alike. ",
+        slug:"react-learn"
     },
     {
         title:"How to get started with Express",
-        content:"Express is a versatile library that is great for beginners and experts alike."
+        content:"Express is a versatile library that is great for beginners and experts alike.",
+        slug:"express-learn"
     }
 ]
 
--- routes/blog.js
+++ routes/blog.js
@@ -8,11 +8,17 @@ router.get('/',(req,res)=>{
 })
 
 router.get('/blog',(req,res)=>{
-    blogs.forEach(e=>{
+    /* blogs.forEach(e=>{
         console.log(e.title)
-    })
+    }) */
     res.sendFile(path.join(__dirname,'../templates/bloghome.html'))
 })
-
+router.get('/blogpost/:slug',(req,res)=>{
+    myBlog=blogs.filter((e)=>{
+        e.slug==req.params.slug
+    })
+    console.log(myBlog)
+    res.sendFile(path.join(__dirname,'../templates/blogPage.html'))
+})
 
 module.exports=router
