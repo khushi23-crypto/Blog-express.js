42df98548a9a66312a0b8b0cbf65c56dcbb05ff2
Kajal Saini <sizukabts91@gmail.com>
2025-09-18 20:28:45 +0530

    finalise blog

--- routes/blog.js
+++ routes/blog.js
@@ -18,7 +18,10 @@ router.get('/blogpost/:slug',(req,res)=>{
     myBlog=blogs.filter((e)=>{
       return  e.slug==req.params.slug
     })
-   
+    res.render('blogPage',{
+    title: myBlog[0].title,
+    content: myBlog[0].content
+   })
     /* console.log(myBlog)
     res.sendFile(path.join(__dirname,'../templates/blogPage.html'))
  */})
