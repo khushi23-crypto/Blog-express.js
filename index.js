41960eed55834450eaf19e61c1dc4203218e82be
Kajal Saini <sizukabts91@gmail.com>
2025-09-18 20:03:00 +0530

    using handlesbars

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')   
+
+
+app.engine('handlebars', exphbs.engine())  
+
+app.set('view engine', 'handlebars');
+
+
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use('/', require(path.join(__dirname,'routes/blog')))
--- routes/blog.js
+++ routes/blog.js
@@ -4,14 +4,15 @@ const path=require('path')
 const blogs=require('../data/blogs')
 
 router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'))
+    //res.sendFile(path.join(__dirname,'../templates/index.html'))
+    res.render('home')
 })
 
 router.get('/blog',(req,res)=>{
-    /* blogs.forEach(e=>{
-        console.log(e.title)
-    }) */
-    res.sendFile(path.join(__dirname,'../templates/bloghome.html'))
+   // res.sendFile(path.join(__dirname,'../templates/bloghome.html'))
+   res.render('blogHome',{
+    blogs:blogs
+   })
 })
 router.get('/blogpost/:slug',(req,res)=>{
     myBlog=blogs.filter((e)=>{
